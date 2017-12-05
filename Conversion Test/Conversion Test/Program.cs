f731c9a883ef343c7f2a4c7b6050987a8ea90597
Ron Aebig <tankstrr@gmail.com>
2017-12-05 15:30:55 -0700

    Changed code to be used for multiple measurements (meters, liters, Units, etc)

--- Conversion Test/Conversion Test/Program.cs
+++ Conversion Test/Conversion Test/Program.cs
@@ -7,29 +7,36 @@ namespace Conversion_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Conversion Test 1.0 by tankstrr");
+            string baseUnit = "";
+            Console.Write("Enter a Number: ");
+            decimal baseNum = Convert.ToDecimal(Console.ReadLine());
+            decimal origNum = baseNum;
+            decimal newNum = baseNum;
+            string newUnit = baseUnit;
             while (true)
             {
-                string baseUnit = "m";
-                Console.Write("Enter a Number: ");
-                decimal baseNum = Convert.ToDecimal(Console.ReadLine());
-                decimal newNum = baseNum;
-                string newUnit = baseUnit;
+                baseNum = baseNum + origNum;
+                newNum = baseNum;
+                newUnit = "";
                 if (baseNum >= 10000)
                 {
                     newNum = baseNum / 1000;
-                    newUnit = "km";
+                    newUnit = "k";
                 }
                 if (baseNum >= 10000000)
                 {
                     newNum = baseNum / 1000000;
-                    newUnit = "Mm";
+                    newUnit = "M";
                 }
                 if (baseNum >= 10000000000)
                 {
                     newNum = baseNum / 1000000000;
-                    newUnit = "Gm";
+                    newUnit = "G";
                 }
-                Console.WriteLine("Converted number is {0:N2} {1}", newNum, newUnit);
+                Console.SetCursorPosition(0, 3);
+                Console.WriteLine("Converted number is {0,12:N2} {1}m", newNum, newUnit);
+
+                //System.Threading.Thread.Sleep(50);
             }
 
         }
