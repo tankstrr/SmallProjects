6bb356f3ddce13d3d51a2f2592066300ee3477d1
Ron Aebig <tankstrr@gmail.com>
2017-12-07 15:26:46 -0700

    Sync failure fixed

--- Conversion Test/Conversion Test/Program.cs
+++ Conversion Test/Conversion Test/Program.cs
@@ -9,36 +9,36 @@ namespace Conversion_Test
             Console.WriteLine("Conversion Test 1.0 by tankstrr");
             string baseUnit = "";
             Console.Write("Enter a Number: ");
-            decimal baseNum = Convert.ToDecimal(Console.ReadLine());
-            decimal origNum = baseNum;
-            decimal newNum = baseNum;
-            string newUnit = baseUnit;
+            double baseNum = Convert.ToDouble(Console.ReadLine());
+            double origNum = baseNum;
             while (true)
             {
-                baseNum = baseNum + origNum;
-                newNum = baseNum;
-                newUnit = "";
-                if (baseNum >= 10000)
-                {
-                    newNum = baseNum / 1000;
-                    newUnit = "k";
-                }
-                if (baseNum >= 10000000)
-                {
-                    newNum = baseNum / 1000000;
-                    newUnit = "M";
-                }
-                if (baseNum >= 10000000000)
-                {
-                    newNum = baseNum / 1000000000;
-                    newUnit = "G";
-                }
                 Console.SetCursorPosition(0, 3);
-                Console.WriteLine("Converted number is {0,12:N2} {1}m", newNum, newUnit);
+                Console.WriteLine("Converted number is {0,12}m", ToSI(baseNum, "##,##0.00"));
+                baseNum = baseNum + origNum;
 
-                System.Threading.Thread.Sleep(10);
+                //System.Threading.Thread.Sleep(100);
             }
 
         }
+
+        static string ToSI(double d, string format = null)
+        {
+            char[] incPrefixes = new[] { 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y' };
+            char[] decPrefixes = new[] { 'm', '\u03bc', 'n', 'p', 'f', 'a', 'z', 'y' };
+
+            int degree = (int)Math.Floor(Math.Log10(Math.Abs(d)) / 3);
+            double scaled = d * Math.Pow(1000, -degree);
+
+            char? prefix = null;
+            switch (Math.Sign(degree))
+            {
+                case 1: prefix = incPrefixes[degree - 1]; break;
+                case -1: prefix = decPrefixes[-degree - 1]; break;
+            }
+
+            return scaled.ToString(format) + " " + prefix;
+        }
+
     }
 }
